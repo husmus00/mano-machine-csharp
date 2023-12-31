8e7d3db229fed1c7018048e808eb1bd55bfe37bb
husmus00 <60274772+husmus00@users.noreply.github.com>
2023-12-31 04:04:21 +0300

    Modified all WriteLine statements and logging system to write to the Interface class instead of System.Console

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,53 @@
 using System;
+using System.IO;
+using Interface;
+
+
+// Serves as a shared interface for seperate implementation between
+// the desktop and web versions of the emulator
+namespace Interface
+{
+    class Console
+    {
+        public Console() { }
+
+        public static void WriteLine()
+        {
+            System.Console.WriteLine();
+        }
+
+        public static void WriteLine(string output)
+        {
+            System.Console.WriteLine(output);
+        }
+
+        public static void Write(string output)
+        {
+            System.Console.Write(output);
+        }
+
+        public static void Clear()
+        {
+            System.Console.Clear();
+        }
+
+        public static void ReadKey()
+        {
+            System.Console.ReadKey();
+        }
+
+        public static string ReadLine()
+        {
+            string? val = System.Console.ReadLine();
+
+            if (val == null) {
+                return "";
+            }
+
+            return val;
+        }
+    }
+}
 
 namespace ManoMachine
 {
@@ -14,5 +63,7 @@ namespace ManoMachine
         {
             Logger.End();
         }
+
+        // Interface.Console.WriteLine();
     }
 }
